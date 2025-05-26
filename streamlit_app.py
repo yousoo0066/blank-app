8c93e65c4b70c7d564dec499b730ef4fe8a76a77
yousoo0066 <yousoo0066@naver.com>
2025-05-26 02:32:23 +0000

    이름변경

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import time
 
-st.title("🎈 안녕하세요.")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.header("한유수")
+
+with st.spinner("전체 작업 진행 중..."):
+    progress = st.progress(0)
+    status_text = st.empty()  # 텍스트 덮어쓰기 용 공간 확보
+
+    for i in range(5):
+        status_text.write(f"🔧 Step {i+1}/5: 데이터 준비 중...")
+        time.sleep(1)
+        progress.progress((i + 1) * 20)
+
+st.success("처리가 모두 끝났습니다!")
+
+
